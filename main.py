17efcd41c7e7ea8736dce1b4c538e28250475429
dek_kn <kratko@ucu.edu.ua>
2019-02-11 22:30:36 +0200

    Commit

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import re
 import folium
 import geocoder
 
-
-
 MESSAGE_GETMAP_INPUT = 'Enter year: '
 MESSAGE_GETMAP_NOMOVIES = 'No movies shoot this year. Press Enter to exit. '
 MESSAGE_GETMAP_DONE = 'Done to map.html. Press Enter to exit. '
@@ -16,10 +14,17 @@ FILENAME_HTML_RESULTGEOMAP = 'map.html'
 
 REGEXP_LINE = r'^".*" \((\d+)\)[^\t]*\t+([^\t]+)'
 
-MAPS_API_KEY = 'At3AyKtDPtkovQwtGL5yqPzuxbnu_siJ6fWeJu8gDJsWCtQ6jeHZpwdQqwM1p9s0'
+MAPS_API_KEY = 'At3AyKtDPtkovQwtGL5yqPzuxbnu_siJ6fWeJu8gDJsWCtQ6jeHZpwdQqwM1p9\
+s0'
 
 
 def input_year(message):
+    '''
+    (str) -> int
+
+    Prompt user with message to enter an integer. If the input cannot be
+    converted to int, return 0.
+    '''
     try:
         return int(input(message))
     except:
@@ -27,6 +32,12 @@ def input_year(message):
 
 
 def create_json_file(input_filename, output_filename):
+    '''
+    (str, str) -> None
+
+    Create json file (output_filename) containing movie studios filmed each
+    year from IMDB locations file (input_filename).
+    '''
     input_file = open(input_filename, 'r')
     while input_file.readline() != '==============\n':
         pass
@@ -34,7 +45,8 @@ def create_json_file(input_filename, output_filename):
     line = ''
     while True:
         line = input_file.readline().strip('\n')
-        if line == '--------------------------------------------------------------------------------':
+        if line == '----------------------------------------------------------\
+----------------------':
             break
         else:
             match = re.match(REGEXP_LINE, line)
@@ -52,18 +64,36 @@ def create_json_file(input_filename, output_filename):
 
 
 def get_coordinates(addresses):
+    '''
+    (list) -> list
+
+    Get coords list from Bing maps from addresses list.
+    '''
     print('in')
     print(len(addresses))
     coords = []
     num_slices_and_rest = divmod(len(addresses), 50)
     for i in range(num_slices_and_rest[0]):
         print('in2')
-        coords.extend([venue.latlng for venue in geocoder.bing(addresses[50 * i : 50 * (i + 1)], method='batch', key=MAPS_API_KEY)])
-    coords.extend([venue.latlng for venue in geocoder.bing(addresses[len(addresses) - num_slices_and_rest[1] : len(addresses)], method='batch', key=MAPS_API_KEY)])
+        coords.extend([venue.latlng for venue in
+                       geocoder.bing(addresses[50 * i: 50 * (i + 1)],
+                                     method='batch', key=MAPS_API_KEY)])
+    coords.extend([venue.latlng for venue in
+                   geocoder.bing(addresses[len(addresses) -
+                                           num_slices_and_rest[1]:
+                                           len(addresses)],
+                                 method='batch', key=MAPS_API_KEY)])
     return coords
 
 
 def main():
+    '''
+    () -> None
+
+    Main program requesting year, creating movie studios json files,
+    collecting coords and creating an HTML map with movie studios filming in
+    the entered year, world countries and US states.
+    '''
     year = 0
     while not year:
         year = input_year(MESSAGE_GETMAP_INPUT)
@@ -82,11 +112,12 @@ def main():
         coords = get_coordinates(venues_list)
         for i in range(len(venues_list)):
             folium.Marker(coords[i], popup=venues_list[i]).add_to(map)
+        countries_geo = 'countries.geo.json'
+        map.choropleth(geo_data=countries_geo)
+        regions_geo = 'us_states_geojson.geojson'
+        map.choropleth(geo_data=regions_geo, fill_color='Reds')
+        map.add_child(folium.map.LayerControl())
         map.save(FILENAME_HTML_RESULTGEOMAP)
-        print(FILENAME_HTML_RESULTGEOMAP)
-
-
-
-#create_json_file('locations.list', 'locations.json')
+        print(MESSAGE_GETMAP_DONE)
 
 main()
